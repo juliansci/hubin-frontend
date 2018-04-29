aa1963ef7c3d31e6c55a7f7124d77275f1d9de78
Julián Scialabba <julian.scialabba@gmail.com>
2018-04-29 11:30:37 -0300

    Start profile edit

--- app/scripts/controllers/user.js
+++ app/scripts/controllers/user.js
@@ -10,10 +10,33 @@
 angular.module('hubinFrontendApp')
   .controller('UserCtrl', function ($rootScope, $scope, $location, userService, securityService) {
     var user = securityService.getUser();
-    userService.getUser(user.id).then(function (data) {
-      console.log(data);
+    userService.getUser(user.id).then(function (response) {
+      $scope.userProfile = response.data;
     }).catch(function (data) {
       console.log(data);
     })
+    $('.js-edit-profile').on('click', function () {
+      $(this).addClass('not-show');
+      $('.js-user-username').editable({
+        type: 'text',
+        pk: 3,
+        title: 'Enter username'
+      });
+      $('.js-user-name').editable({
+        type: 'text',
+        pk: 3,
+        title: 'Enter name'
+      });
+      $('.js-user-email').editable({
+        type: 'text',
+        pk: 3,
+        title: 'Enter email'
+      });
+      $('.js-user-description').editable({
+        type: 'text',
+        pk: 3,
+        title: 'Enter description'
+      })
+    });
 
   });
